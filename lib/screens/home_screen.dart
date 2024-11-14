790b79094e6c7ee9343b39f3f1ddbb4a79881f8f
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-11-14 14:35:11 +0900

    update on button,padding, and app bar color

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,11 +15,12 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
   String _filter = 'All';
   String _sortOrder = 'Newest';
+  int _emptyAddTaskClickCount = 0; // Counter for empty clicks
 
   @override
   void initState() {
     super.initState();
-    _loadTasks(); // Load tasks when the screens initializes
+    _loadTasks();
   }
 
   Future<void> _loadTasks() async {
@@ -39,10 +40,10 @@ class _HomeScreenState extends State<HomeScreen> {
         });
       } else {
         debugPrint("File does not exist. Creating a new one.");
-        await file.create(); // Create a new file if it doesn't exist
+        await file.create();
       }
     } catch (e) {
-      debugPrint("Error loading tasks: $e"); // Log the error
+      debugPrint("Error loading tasks: $e");
     }
   }
 
@@ -54,9 +55,9 @@ class _HomeScreenState extends State<HomeScreen> {
         return '${task.title}|${task.isDone}';
       }).toList();
       await file.writeAsString(taskList.join('\n'));
-      debugPrint("Tasks saved successfully."); // Use debugPrint for logging
+      debugPrint("Tasks saved successfully.");
     } catch (e) {
-      debugPrint("Error saving tasks: $e"); // Use debugPrint for logging
+      debugPrint("Error saving tasks: $e");
     }
   }
 
@@ -65,15 +66,47 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         _tasks.add(Task(title: _controller.text));
         _controller.clear();
-        _saveTasks(); // Save tasks after adding
+        _saveTasks();
+        _emptyAddTaskClickCount = 0;
       });
+    } else {
+      _emptyAddTaskClickCount++;
+      if (_emptyAddTaskClickCount >= 10) {
+        _showEasterEggAnimation();
+        _emptyAddTaskClickCount = 0;
+      }
     }
+  }
+
+  void _showEasterEggAnimation() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: const Text(
+              '☟︎♏︎●︎◻︎ ❍︎♏︎✏︎ ✋︎ ♋︎❍︎ ⧫︎❒︎♋︎◻︎ ♒︎♏︎❒︎♏︎✏︎ 💧︎□︎❍︎♏︎□︎■︎♏︎ ●︎♏︎⧫︎ ❍︎♏︎ □︎◆︎⧫︎✏︎'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _deleteTask(int index) {
     setState(() {
       _tasks.removeAt(index);
-      _saveTasks(); // Save tasks after deleting
+      _saveTasks();
+    });
+  }
+
+  void _editTask(int index, String newTitle) {
+    setState(() {
+      _tasks[index].title = newTitle;
+      _saveTasks();
     });
   }
 
@@ -81,7 +114,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return _tasks.where((task) {
       if (_filter == 'Finished') return task.isDone;
       if (_filter == 'Unfinished') return !task.isDone;
-      return true; // All
+      return true;
     }).toList();
   }
 
@@ -93,107 +126,179 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60.0), // Set height for AppBar
-        child: Container(
-          padding: const EdgeInsets.all(10.0), // Added padding to AppBar
-          child: AppBar(
-            title: const Text('To-Do List'),
-            actions: [
-              DropdownButton<String>(
-                value: _filter,
-                icon: const Icon(Icons.check), // Check button icon only
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _filter = newValue!;
-                  });
-                },
-                items: <String>['All', 'Finished', 'Unfinished']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Row(
-                      children: [
-                        Text(value), // Text for the dropdown item
-                      ],
-                    ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(width: 16.0), // Add space between the two buttons
-              DropdownButton<String>(
-                value: _sortOrder,
-                icon: const Icon(Icons.sort), // Sort button icon only
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _sortOrder = newValue!;
-                  });
-                },
-                items: <String>['Newest', 'Oldest']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Row(
-                      children: [
-                        Text(value), // Text for the dropdown item
-                      ],
-                    ),
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Padding(
-        padding:
-        const EdgeInsets.all(10.0), // Added padding of 10 pixels to body
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      labelText: 'Enter a task',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                    width: 10.0), // Space between TextField and button
-                ElevatedButton(
-                  onPressed: _addTask,
-                  child: const Text('Add Task'),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60.0),
+          child: Container(
+            padding: const EdgeInsets.only(left: 10.0, right: 20.0, top: 10.0),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 226, 109, 0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  spreadRadius: 0.5,
+                  blurRadius: 10,
                 ),
               ],
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: sortedTasks.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(sortedTasks[index].title),
-                    leading: Checkbox(
-                      value: sortedTasks[index].isDone,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          sortedTasks[index].isDone = value!;
-                        });
-                      },
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () =>
-                          _deleteTask(_tasks.indexOf(sortedTasks[index])),
-                    ),
-                  );
-                },
-              ),
+            child: AppBar(
+              title: const Text('TDL'),
+              backgroundColor: const Color.fromARGB(255, 226, 109, 0),
+              foregroundColor: Colors.white,
+              actions: [
+                DropdownButton<String>(
+                  value: _filter,
+                  icon: const Icon(Icons.check, color: Colors.white),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _filter = newValue!;
+                    });
+                  },
+                  items: <String>['All', 'Finished', 'Unfinished']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(width: 16.0),
+                DropdownButton<String>(
+                  value: _sortOrder,
+                  icon: const Icon(Icons.sort, color: Colors.white),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _sortOrder = newValue!;
+                    });
+                  },
+                  items: <String>['Newest', 'Oldest']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
             ),
-          ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 30.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0, right: 0.0),
+                      child: TextField(
+                        controller: _controller,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter a task',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: ElevatedButton(
+                      onPressed: _addTask,
+                      child: const Text('Add Task'),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  itemCount: sortedTasks.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(sortedTasks[index].title),
+                      leading: Checkbox(
+                        value: sortedTasks[index].isDone,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            sortedTasks[index].isDone = value!;
+                          });
+                        },
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () {
+                              // Show a dialog to edit the task
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  final TextEditingController editController =
+                                      TextEditingController(
+                                          text: sortedTasks[index].title);
+                                  return AlertDialog(
+                                    title: const Text('Edit Task'),
+                                    content: TextField(
+                                      controller: editController,
+                                      decoration: const InputDecoration(
+                                          labelText: 'Task Title'),
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(),
+                                        child: const Text('Cancel'),
+                                      ),
+                                      TextButton(
+                                        style: TextButton.styleFrom(
+                                          backgroundColor: Colors.blue,
+                                          foregroundColor: Colors.white,
+                                        ),
+                                        onPressed: () {
+                                          _editTask(
+                                              _tasks
+                                                  .indexOf(sortedTasks[index]),
+                                              editController.text);
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text('Save'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () =>
+                                _deleteTask(_tasks.indexOf(sortedTasks[index])),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
